401b2c8b1e569cb49c82d3bc8ebc75c9a00622be
Stefi Stoeva <44346648+stefistoeva@users.noreply.github.com>
2019-05-30 14:05:07 +0300

    Update 09. Employee Departments.sql

--- 04.Database-Relations-and-Design/09. Employee Departments.sql
+++ 04.Database-Relations-and-Design/09. Employee Departments.sql
@@ -2,7 +2,7 @@ SELECT
 	e.employee_id, 
 	e.first_name, 
 	e.salary, 
-	d.`name`
+	d.`name` AS `department_name`
 FROM employees AS e
 LEFT JOIN departments AS d ON e.department_id = d.department_id
 WHERE e.salary > 15000
