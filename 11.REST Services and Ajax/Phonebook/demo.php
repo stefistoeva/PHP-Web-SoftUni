179b20ef667980a3b913084b7a18ab4e97603674
Stefi Stoeva <44346648+stefistoeva@users.noreply.github.com>
2019-06-26 22:47:54 +0300

    Create demo.php

---
+++ 11.REST Services and Ajax/Phonebook/demo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Phonebook</title>
+    <script src="../script/jquery-minified-3.4.1.js"></script>
+</head>
+<body>
+<h1>Phonebook</h1>
+<ul id="phonebook"></ul>
+<button id="btnLoad">Load</button>
+<h2>Create Contact</h2>
+Person: <input type="text" id="person" />
+<br>
+Phone: <input type="text" id="phone" />
+<br>
+<button id="btnCreate">Create</button>
+<script src="../script/phonebook.js"></script>
+</body>
+</html>
