16977f0cd9b0bc20448884e62fcf7a73f06266b9
Stefi Stoeva <44346648+stefistoeva@users.noreply.github.com>
2019-06-26 14:30:40 +0300

    Create ErrorDTO.php

---
+++ 10.WORKSHOP: CREATING AN ORM\VIEW ENGINE/App/Data/ErrorDTO.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Data;
+
+class ErrorDTO
+{
+    private $message;
+
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+}
